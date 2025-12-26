9267b5c2d7cead88fd0973fcd4e21867b94f25dc
vanshth17 <vanshth17@gmail.com>
2025-12-26 17:39:10 +0530

    phase 3 completed:tickets and replys core model

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ticket , Reply
+
+admin.site.register(Ticket)
+admin.site.register(Reply)
--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TicketStatus(models.TextChoices):
+    OPEN = 'OPEN' , 'Open'
+    IN_PROGRESS = 'IN_PROGRESS' , 'In progress'
+    RESOLVED = 'RESOLVED' , 'Resolved'
+    CLOSED = 'CLOSED', 'Closed'
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    status = models.CharField(
+        max_length=20,
+        choices = TicketStatus.choices,
+        default = TicketStatus.OPEN
+    )
+
+    created_by = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name = 'created_tickets'
+    )
+
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete = models.SET_NULL,
+        null = True,
+        blank = True,
+        related_name = 'assigned_tickets'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.status})"
+    
+
+class Reply(models.Model):
+    ticket = models.ForeignKey(
+        Ticket,
+        on_delete=models.CASCADE,
+        related_name='replies'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='replies'
+    )
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Reply by {self.author.username}"
